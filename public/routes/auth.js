5d2e63f2ff4b8880faa6af05a6194711d9479228
Anuragsinghsengar <131814714+Anuragsinghsengar@users.noreply.github.com>
2024-09-03 20:32:39 +0530

    railway app

---
+++ public/routes/auth.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "heisagoodboy";
+var fetchuser = require('../middleware/fetchuser');
+
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a name minimun 3 characters").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password should be minimum 3 characters").isLength({
+      min: 3,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = await validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      let user = await User.findOne({ email: req.body.email });
+      console.log(user);
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = await validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct crediantials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct crediantials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some Error occured");
+    }
+  }
+);
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    onsole.log(error.message);
+    res.status(500).send("Some Error occured");
+  }
+});
+
+module.exports = router;
